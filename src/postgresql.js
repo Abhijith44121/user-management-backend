f3661b4ca24031b98f34596fbdb30d02bbad3b29
Abhijith44121 <abhijithabhi508108@gmail.com>
2026-03-19 23:47:35 +0530

    fix: add SSL to knex and postgresql connection

--- src/postgresql.js
+++ src/postgresql.js
@@ -1,11 +1,14 @@
 import knex from 'knex'
 
 export const postgresql = async (app) => {
-  console.log("DB URL:", process.env.DATABASE_URL) // 👈 for debugging
+  console.log("DB URL:", process.env.DATABASE_URL)
 
   const db = knex({
     client: 'pg',
-    connection: process.env.DATABASE_URL  // ✅ THIS LINE
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    }
   })
 
   app.set('postgresqlClient', db)
